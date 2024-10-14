0ebd73a4ed79106e650394a5fa35dc30388d9e26
Jose Junior <jlfjunior8@gmail.com>
2024-10-14 20:40:06 -0300

    enhancement: adjust module structure

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import sort "github.com/jlfjunior/learning-golang/algorithms"
+import sort "github.com/jlfjunior/learning-golang/sort"
 
 func main() {
 	numbers := []int{9, 0, 3, 7, 5, 2, 8, 4, 6, 1}
--- sort/quickSort.go
+++ sort/quickSort.go
@@ -1,4 +1,4 @@
-package algorithms
+package sort
 
 func QuickSort(numbers []int, low int, high int) {
 
